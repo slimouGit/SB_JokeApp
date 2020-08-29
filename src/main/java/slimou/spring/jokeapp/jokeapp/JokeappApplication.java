fc017a50bc46acdee26d2f2c5bcd7cf3ff787288
Salim Oussayfi <box@slimou.net>
2020-08-29 14:55:50 +0200

    JokeApp

---
+++ src/main/java/slimou/spring/jokeapp/jokeapp/JokeappApplication.java
@@ -0,0 +1,13 @@
+package slimou.spring.jokeapp.jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeappApplication.class, args);
+    }
+
+}
