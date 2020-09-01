b2069f5ab3dafa4f7b6643a2d198193a7f5e7b8e
Salim Oussayfi <box@slimou.net>
2020-09-01 18:22:09 +0200

    Spring XML Configuration

--- src/main/java/slimou/spring/jokeapp/jokeapp/config/ChuckConfiguration.java
+++ src/main/java/slimou/spring/jokeapp/jokeapp/config/ChuckConfiguration.java
@@ -1,17 +1,15 @@
 package slimou.spring.jokeapp.jokeapp.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * User: salim
  * Date: 01.09.2020 18:02
  */
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-    @Bean
+    //@Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
